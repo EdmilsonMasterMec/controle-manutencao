82814f5e38cc2ddecc53e1902eff42bae017fb57
EdmilsonMasterMec <126733949+EdmilsonMasterMec@users.noreply.github.com>
2026-09-20 14:44:14 -0300

    Atualizar o supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,15 +1,10 @@
-
 import { createClient } from "@supabase/supabase-js";
 
-// Conexão com o projeto Supabase
-const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabasePublishableKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
-const supabaseKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-// Cliente utilizado pelo sistema MasterMec
 export const supabase = createClient(
   supabaseUrl,
-  supabaseKey
+  supabasePublishableKey
 );
